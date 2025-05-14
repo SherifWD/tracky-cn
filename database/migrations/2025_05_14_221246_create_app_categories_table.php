aca3e38e49956fadcdbd030bfcc92c89018de853
SherifWD <sherifshahawywd@gmail.com>
2025-05-15 02:07:31 +0300

    edit

--- database/migrations/2025_05_14_221246_create_app_categories_table.php
+++ database/migrations/2025_05_14_221246_create_app_categories_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('app_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
