08afc6c172f9db9b633c41da82c64d7504c40219
SherifWD <sherifshahawywd@gmail.com>
2025-06-19 09:56:29 +0000

    Filament Config

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * The path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
+    /**
+     * Define your route model bindings, pattern filters, and other route configuration.
+     */
+    public function boot(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+          Livewire::setUpdateRoute(function($handle){
+              $url = '/tracky-cn/public/livewire/update';
+              return Route::post($url,$handle);
+          });
+    }
+}
