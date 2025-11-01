7594aeb447e55e80e8753d504d9a312e212ec767
SherifWD <sherifshahawywd@gmail.com>
2025-11-01 22:08:19 +0200

    Edit

--- app/Http/Controllers/Api/SubscriptionController.php
+++ app/Http/Controllers/Api/SubscriptionController.php
@@ -118,7 +118,7 @@ public function searchShippment(Request $request)
             'carrierCd' => $request->input('carrier_code')
         ]]);
         $result = $searchResponse->json();
-        if ($result['code'] !== 200 || empty($result['result'])) {
+        if (empty($result['result'])) {
             return response()->json([
                 'code' => 404,
                 'message' => 'Shipment not found'
