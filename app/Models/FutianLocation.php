bd40eb071359faae9852c5b443c9eccc54d7a58b
SherifWD <sherifshahawywd@gmail.com>
2026-03-18 20:45:02 +0200

    Fx

--- app/Models/FutianLocation.php
+++ app/Models/FutianLocation.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class FutianLocation extends Model
 {
+    protected $guarded =[];
      public function getImageAttribute($val)
     {
         return ($val !== null) ? asset('/'.$val) : "";
